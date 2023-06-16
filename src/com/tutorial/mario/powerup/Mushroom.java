5f1bf6ff4a2f9f65b5dcb408811cef71fd72f7ce
ntyl2 <ndnq0001@gmail.com>
2023-06-16 17:13:23 +0700

    Mush

---
+++ src/com/tutorial/mario/powerup/Mushroom.java
@@ -0,0 +1,28 @@
+package com.tutorial.mario.powerup;
+
+import java.awt.Graphics;
+
+import com.tutorial.mario.Game;
+import com.tutorial.mario.Handler;
+import com.tutorial.mario.Id;
+import com.tutorial.mario.entity.Entity;
+
+public abstract class Mushroom extends Entity{
+
+	public Mushroom(int x, int y, int width, int height, boolean solid, Id id, Handler handler) {
+		super(x, y, width, height, solid, id, handler);
+		
+	}
+	public void render(Graphics g)
+	{
+		g.drawImage(Game.mushroom.getBufferedImage(), x, y, width, height, null);
+	}
+	
+	public void tick()
+	{
+		x+=velX;
+		y+=velY;
+	}
+	
+
+}
